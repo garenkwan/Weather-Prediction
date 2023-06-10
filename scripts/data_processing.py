8f0e718f665968dcb7cee775af4e8b9a4d605763
garenkwan <60970926+garenkwan@users.noreply.github.com>
2023-06-11 03:27:48 +0800

    data_processing.py debugging errors add docstrings

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -8,6 +8,9 @@ with open("scripts/station_ids.yml", "r") as stream:
     station_ids = yaml.safe_load(stream)
 
 class weatherData:
+    '''
+    Creates iterable dataset that provides a 3D training matrix and 1D prediction array
+    '''
     def __init__(self, 
                  start_date="2021-10-15",
                  end_date="2021-10-18", 
@@ -19,7 +22,7 @@ class weatherData:
         self.forecast=forecast
         self.station_ids = station_ids
 
-    def data_fill_missing(self, df, date):
+    def __data_fill_missing__(self, df, date):
         hours = set([x for x in range(24)])
         cols = set([x.hour for x in df.columns])
         missing = hours.difference(cols)
@@ -42,13 +45,13 @@ class weatherData:
         ordered_columns = [date + pd.to_timedelta(x, unit='h') for x in range(24)]
         return df[ordered_columns]
         
-    def data_transpose(self, df, data_type):
+    def __data_transpose__(self, df, data_type):
         df['timestamp'] = pd.to_datetime(df['date']) + pd.to_timedelta(df['time'], unit='h')
         df_T = df.loc[(df['data_type']==data_type), ['station_id', 'timestamp', 'value']]\
                 .pivot(index='station_id', columns='timestamp', values='value')
         return df_T
         
-    def df_preprocessing(self, df, data_type):
+    def __df_preprocessing__(self, df, data_type):
         df['timestamp'] = pd.to_datetime(df['timestamp'])
         df['date'] = df['timestamp'].apply(lambda x: x.date)
         df['time'] = df['timestamp'].apply(lambda x: x.hour)
@@ -63,36 +66,54 @@ class weatherData:
         return df_grouped
 
     def get_data(self, date):
-        data_types = ['environment/rainfall', 
-                    'environment/relative-humidity',
-                    'environment/wind-direction', 
-                    'environment/wind-speed',
-                    'environment/air-temperature']
+        '''
+        Helper function to get a list of 5 dataframes that are used for training
+        The 5 dataframes are arranged in the following order:
+        result[0] : 'environment/rainfall'
+        result[1] : 'environment/relative-humidity'
+        result[2] : 'environment/wind-direction'
+        result[3] : 'environment/wind-speed'
+        result[4] : 'environment/air-temperature'
+        '''
+        data_types = ['environment/rainfall',
+                      'environment/relative-humidity',
+                      'environment/wind-direction',
+                      'environment/wind-speed',
+                      'environment/air-temperature']
         data = pd.read_csv("./data/"+date+".csv")
 
         result = []
         for data_type in data_types:
             df_index = pd.DataFrame(self.station_ids, columns=['station_id']).set_index('station_id')
-            df_process = self.df_preprocessing(data, data_type)
-            df_T = self.data_transpose(df_process, data_type)            
-            df_T = self.data_fill_missing(df_T, date)
+            df_process = self.__df_preprocessing__(data, data_type)
+            df_T = self.__data_transpose__(df_process, data_type)            
+            df_T = self.__data_fill_missing__(df_T, date)
             final = df_index.join(df_T).fillna(0.0)
             result.append(final)
         return result
 
-    def training_data(self, start_date, end_date, training_window, forecast):
+    def training_data(self, 
+                      start_date="", 
+                      end_date="", 
+                      training_window=0, 
+                      forecast=0):
         '''
         Prepares a list of 3D arrays and predictor
         training_window: number of hours that should be used X
         forecast: number of hourst that should be used as Y
         '''
+        if start_date == "":
+            start_date = self.start_date
+        if end_date == "":
+            end_date = self.end_date
+        if training_window == 0:
+            training_window = self.training_window
+        if forecast == 0:
+            forecast = self.forecast
 
         start_date = pd.to_datetime(start_date)
         end_date = pd.to_datetime(end_date)
         window_size = training_window + forecast
-
-        training_data = []
-        predict_data = []
         
         days = (end_date - start_date).days
         date_range = [(start_date + pd.to_timedelta(x, unit='d')).strftime('%Y-%m-%d')\
@@ -103,15 +124,15 @@ class weatherData:
         # This should prepare the basic df
         if days_per_window > 0 and days_per_window < days:
             df_list = self.get_data(date_range.pop(0))
-            days_per_window -= 1
-            days -= 1
-            while days_per_window >= 0:
+
+            while days_per_window > 0:
                 extend_list = self.get_data(date_range.pop(0))
                 for i, df in enumerate(extend_list):
-                    df_list[i] = pd.concat([df_list[0], df], axis=1)
+                    df_list[i] = pd.concat([df_list[i], df], axis=1)
                 days_per_window -= 1
                 days -= 1
         else:
+            #TODO: error handling when days_per_window > days
             df_list = self.get_data(date_range.pop(0))
         
         logging.debug(date_range)
@@ -122,26 +143,24 @@ class weatherData:
         while days > 0 or len(working_columns) >= window_size:
             while window_size <= len(working_columns):
                 train_columns = working_columns[:training_window]
-                predict_column = working_columns[training_window:training_window+forecast]
-                
+                predict_column = working_columns[training_window:window_size]
                 training_data = np.array(
-                                                [
-                                                np.array(df_list[0][train_columns]),
-                                                np.array(df_list[1][train_columns]),
-                                                np.array(df_list[2][train_columns]),
-                                                np.array(df_list[3][train_columns]),
-                                                np.array(df_list[4][train_columns])
-                                                ]
+                                            (
+                                            df_list[0][train_columns].to_numpy(),
+                                            df_list[1][train_columns].to_numpy(),
+                                            df_list[2][train_columns].to_numpy(),
+                                            df_list[3][train_columns].to_numpy(),
+                                            df_list[4][train_columns].to_numpy()
                                             )
-                predict_data = np.sum(np.array(df_list[0][predict_column]), axis=1)
-            
+                                        )
+                predict_data = np.sum(df_list[0][predict_column].to_numpy(), axis=1)
                 working_columns.pop(0)
-                yield training_data, predict_data
+                yield (training_data, predict_data)
 
             if days > 0:
                 extend_list = self.get_data(date_range.pop(0))
                 for i, df in enumerate(extend_list):
-                    df_list[i] = pd.concat([df_list[0], df], axis=1)
+                    df_list[i] = pd.concat([df_list[i], df], axis=1)
                 working_columns = list(df_list[0].columns)
                 days -= 1
 
